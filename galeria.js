5781b2dea120e6bfce32d1ec96af3e3fba68b2e9
Lucca Nunes <lyulucca@gmail.com>
2022-04-06 18:15:21 -0300

    Galeria de fotos v1.0

---
+++ galeria.js
@@ -0,0 +1,18 @@
+let imagens= document.querySelectorAll('.small_img');
+let modal= document.querySelector('.modal');
+let modalImg= document.querySelector('#modal_img');
+let btnClose= document.querySelector('#btn_close');
+let srcVal='';
+
+for(let i =0; i<imagens.length; i++){
+    imagens[i].addEventListener('click', function(){
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal_active');
+    });
+}
+
+
+btnClose.addEventListener('click', function(){
+    modal.classList.toggle('modal_active');
+    });
